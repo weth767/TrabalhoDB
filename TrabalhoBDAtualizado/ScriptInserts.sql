8757736c28e53f69db9fa56451bd71f827746de1
Weth <joao_paulo767@hotmail.com>
2018-11-15 15:10:57 -0200

    Inserts dos clientes já realizado, falta os fornecedores, funcionarios e produtos

--- TrabalhoBDAtualizado/ScriptInserts.sql
+++ TrabalhoBDAtualizado/ScriptInserts.sql
@@ -1,3 +1,4 @@
+--Inserts dos clientes
 insert into cliente(cliNome,cliCPF, cliRG,cliTelefone, cliCelular, cliEstado, cliCidade, cliBairro, cliRua, cliNumero)
 values("João Paulo de Souza","11111111111","158474524",null,"44999004897","Paraná","Umuarama","Jardim Carolina","Mundo Novo",3474);
 insert into cliente(cliNome,cliCPF, cliRG,cliTelefone, cliCelular, cliEstado, cliCidade, cliBairro, cliRua, cliNumero)
@@ -7,34 +8,35 @@ values("Paulo Henrique Xavier","33333333333","1523245323",null,"37998420784","Mi
 insert into cliente(cliNome,cliCPF, cliRG,cliTelefone, cliCelular, cliEstado, cliCidade, cliBairro, cliRua, cliNumero)
 values("Jefferson Marques","44444444444","198741234",null,"37991548742","Minas Gerais","Arcos","Bigodudo","Rua do Bigóde",112);
 insert into cliente(cliNome,cliCPF, cliRG,cliTelefone, cliCelular, cliEstado, cliCidade, cliBairro, cliRua, cliNumero)
-values("Belão Mateus Fernandes","55555555555","987874514",null,"15987454125","São Paulo","Osasco","Jardim Roubo","Rua dos ladrões",157);
+values("Belão Mateus Fernandes","55555555555","987874514",null,"15987454125","São Paulo","Osasco","Jardim Roubo","Rua dos ladrões",857);
 insert into cliente(cliNome,cliCPF, cliRG,cliTelefone, cliCelular, cliEstado, cliCidade, cliBairro, cliRua, cliNumero)
-values("Thalão Henrique","655621312343","123422341",null,"18987412154","São Paulo","Cruzeiro","Itapemirim","Rua das Árvores",1452);
+values("Thalão Henrique","65562131234","123422341",null,"18987412154","São Paulo","Cruzeiro","Itapemirim","Rua das Árvores",4452);
 insert into cliente(cliNome,cliCPF, cliRG,cliTelefone, cliCelular, cliEstado, cliCidade, cliBairro, cliRua, cliNumero)
 values("Júlio Roberto Pereira","77777777777","169874562",null,"22982369878","Mato Grosso do Sul","Campo Grande","Jardim Matagal","Rua do Mato",1354);
 insert into cliente(cliNome,cliCPF, cliRG,cliTelefone, cliCelular, cliEstado, cliCidade, cliBairro, cliRua, cliNumero)
-values("João Paulo de Souza","11111111111","158474524",null,"37999004897","Paraná","Umuarama","Jardim Carolina","Mundo Novo",3474);
+values("Alfredo Pereira Gomes","88888888888","432341234",null,"21978456989","Rio de Janeiro","Rio de Janeiro","Morro do Alemão","Rua Tiro pro Alto",177);
 insert into cliente(cliNome,cliCPF, cliRG,cliTelefone, cliCelular, cliEstado, cliCidade, cliBairro, cliRua, cliNumero)
-values("João Paulo de Souza","11111111111","158474524",null,"37999004897","Paraná","Umuarama","Jardim Carolina","Mundo Novo",3474);
+values("Fábio Coutinho da Silva","99999999999","987844562",null,"51965487823","Rio Grande do Sul","Porto Alegre","Jardim das Rosas","Deodoro da Fonseca",8798);
 insert into cliente(cliNome,cliCPF, cliRG,cliTelefone, cliCelular, cliEstado, cliCidade, cliBairro, cliRua, cliNumero)
-values("João Paulo de Souza","11111111111","158474524",null,"37999004897","Paraná","Umuarama","Jardim Carolina","Mundo Novo",3474);
+values("Joaquim Santos Domingues","12345678911","654879631",null,"92987852332","Amazonas","Manaus","Feliz Final","Rua do Terraço",268);
 insert into cliente(cliNome,cliCPF, cliRG,cliTelefone, cliCelular, cliEstado, cliCidade, cliBairro, cliRua, cliNumero)
-values("João Paulo de Souza","11111111111","158474524",null,"37999004897","Paraná","Umuarama","Jardim Carolina","Mundo Novo",3474);
+values("Marcos Guimarães Faria","78912345678","564587823",null,"71965874521","Bahia","Salvador","Praia do Calorão","Sol do Meio Dia",6542);
 insert into cliente(cliNome,cliCPF, cliRG,cliTelefone, cliCelular, cliEstado, cliCidade, cliBairro, cliRua, cliNumero)
-values("João Paulo de Souza","11111111111","158474524",null,"37999004897","Paraná","Umuarama","Jardim Carolina","Mundo Novo",3474);
+values("Wellington Ferreira Prado","45698715478","789874568",null,"62954789555","Goiás","Goiânia","Jardim Flor de Outubro","Rua das Palmeiras",7654);
 insert into cliente(cliNome,cliCPF, cliRG,cliTelefone, cliCelular, cliEstado, cliCidade, cliBairro, cliRua, cliNumero)
-values("João Paulo de Souza","11111111111","158474524",null,"37999004897","Paraná","Umuarama","Jardim Carolina","Mundo Novo",3474);
+values("João Victor da Silva Nunes","96325147230","897845216",null,"37999004897","Minas Gerais","Divinópolis","Jardim Laveira","Novo Horizonte",3474);
 insert into cliente(cliNome,cliCPF, cliRG,cliTelefone, cliCelular, cliEstado, cliCidade, cliBairro, cliRua, cliNumero)
-values("João Paulo de Souza","11111111111","158474524",null,"37999004897","Paraná","Umuarama","Jardim Carolina","Mundo Novo",3474);
+values("Ricardo Antunes Lacerda","78925874112","865287412",null,"48999004897","Santa Catarina","Florianópolis","Ouro Branco","José Ferreira",8975);
 insert into cliente(cliNome,cliCPF, cliRG,cliTelefone, cliCelular, cliEstado, cliCidade, cliBairro, cliRua, cliNumero)
-values("João Paulo de Souza","11111111111","158474524",null,"37999004897","Paraná","Umuarama","Jardim Carolina","Mundo Novo",3474);
+values("Roger Moraes Nobrega","12234565411","458795621",null,"65963214587","Mato Grosso","Cuiabá","Vila Formosa","São Paulo",0983);
 insert into cliente(cliNome,cliCPF, cliRG,cliTelefone, cliCelular, cliEstado, cliCidade, cliBairro, cliRua, cliNumero)
-values("João Paulo de Souza","11111111111","158474524",null,"37999004897","Paraná","Umuarama","Jardim Carolina","Mundo Novo",3474);
+values("Miguel Figueiredo Dias","14785236933","625478124",null,"91974589875","Pará","Belém","Vila Carminha","Nunes Filho",1654);
 insert into cliente(cliNome,cliCPF, cliRG,cliTelefone, cliCelular, cliEstado, cliCidade, cliBairro, cliRua, cliNumero)
-values("João Paulo de Souza","11111111111","158474524",null,"37999004897","Paraná","Umuarama","Jardim Carolina","Mundo Novo",3474);
+values("Lucas Pacheco Queiroz","87965412365","745236549",null,"82965412598","Alagoas","Maceió","Santos Dumont","Monte Carlo",0985);
 insert into cliente(cliNome,cliCPF, cliRG,cliTelefone, cliCelular, cliEstado, cliCidade, cliBairro, cliRua, cliNumero)
-values("João Paulo de Souza","11111111111","158474524",null,"37999004897","Paraná","Umuarama","Jardim Carolina","Mundo Novo",3474);
+values("Bernardo Teles Sampaio ","45169878125","918632529",null,"61998741258","Distrito Federal","Brasília","Novo Hamburgo","Jánio Quadros",0973);
 insert into cliente(cliNome,cliCPF, cliRG,cliTelefone, cliCelular, cliEstado, cliCidade, cliBairro, cliRua, cliNumero)
-values("João Paulo de Souza","11111111111","158474524",null,"37999004897","Paraná","Umuarama","Jardim Carolina","Mundo Novo",3474);
+values("Adalberto Novaes Santos","15423216532","895148796",null,"68923479632","Acre","Rio Branco","Boa Esperança","Iguaçu",753);
 insert into cliente(cliNome,cliCPF, cliRG,cliTelefone, cliCelular, cliEstado, cliCidade, cliBairro, cliRua, cliNumero)
-values("João Paulo de Souza","11111111111","158474524",null,"37999004897","Paraná","Umuarama","Jardim Carolina","Mundo Novo",3474);
+values("Daniel Soares Filho","25896312457","621478759",null,"85954877896","Ceará","Fortaleza","Alto da Serra","Veneza",8542);
+--Outros inserts abaixo
